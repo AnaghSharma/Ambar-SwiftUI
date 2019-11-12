ba9d60339866e2850ddbfc016905decad70ce10d
Anagh Sharma <anaghsharma12@gmail.com>
2019-11-12 21:02:16 +0530

    Added EventMonitor to automatically hide the popover on mouse events

---
+++ Ambar/Helpers/EventMonitor.swift
@@ -0,0 +1,41 @@
+//
+//  EventMonitor.swift
+//  Ambar
+//
+//  Created by Anagh Sharma on 12/11/19.
+//  Copyright © 2019 Golden Chopper. All rights reserved.
+//
+
+import Cocoa
+
+class EventMonitor
+{
+    private var monitor: Any?
+    private let mask: NSEvent.EventTypeMask
+    private let handler: (NSEvent?) -> Void
+    
+    public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void)
+    {
+      self.mask = mask
+      self.handler = handler
+    }
+
+    deinit
+    {
+      stop()
+    }
+
+    public func start()
+    {
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler) as! NSObject
+    }
+
+    public func stop()
+    {
+      if monitor != nil
+      {
+        NSEvent.removeMonitor(monitor!)
+        monitor = nil
+      }
+    }
+}
--- Ambar/Helpers/StatusBarController.swift
+++ Ambar/Helpers/StatusBarController.swift
@@ -14,6 +14,7 @@ class StatusBarController
     private var statusItem: NSStatusItem
     private var popover: NSPopover
     private var statusBarButton: NSStatusBarButton
+    private var eventMonitor: EventMonitor?
     
     init(_ popover: NSPopover)
     {
@@ -28,6 +29,8 @@ class StatusBarController
         
         statusBarButton.action = #selector(togglePopover(sender:))
         statusBarButton.target = self
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown], handler: mouseEventHandler)
     }
     
     @objc func togglePopover(sender: AnyObject)
@@ -45,11 +48,13 @@ class StatusBarController
     func showPopover(_ sender: AnyObject)
     {
         popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: NSRectEdge.maxY)
+        eventMonitor?.start()
     }
     
     func hidePopover(_ sender: AnyObject)
     {
         popover.performClose(sender)
+        eventMonitor?.stop()
     }
     
     func mouseEventHandler(_ event: NSEvent?)
