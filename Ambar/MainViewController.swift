7b7b03deccde027cead3c40cdb370acd238c4b5e
Anagh Sharma <anaghsharma12@gmail.com>
2019-11-12 20:38:42 +0530

    Added a view controller to handle popup display

---
+++ Ambar/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  Ambar
+//
+//  Created by Anagh Sharma on 12/11/19.
+//  Copyright © 2019 Golden Chopper. All rights reserved.
+//
+
+import AppKit
+
+class MainViewController: NSViewController
+{
+    
+}
